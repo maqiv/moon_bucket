72e7122eece639e0fe0041b9922aabab2c19914f
maqiv <sebastian.glinski@gmail.com>
2013-06-03 00:19:53 +0200

    first alpha release

---
+++ WebContent/sub/main.h
@@ -0,0 +1,42 @@
+		<div class="poetry-container">
+			<span class="headline">Poetries</span>
+			<hr />
+			<div class="poetry-container-content">
+				<div class="poetry-container-entry">
+					<div class="poetry-container-entry-head">My Poetry</div>
+					<div class="poetry-container-entry-content">Lorem ipsum, bla
+						bla blab bla</div>
+					<div class="poetry-container-entry-foot">Created on
+						13-04-2013</div>
+				</div>
+			</div>
+		</div>
+		<div class="poetry-editor">
+			<span class="headline" data-toggle="collapse" data-target="#colme">Editor</span>
+			<hr />
+			<div class="poetry-editor-content" id="colme">
+				<form>
+					<div class="editor-left">
+						<input type="text" class="editor-subject"><br />
+						<textarea class="editor-corpus"></textarea>
+						<br />
+						<div class="editor-timestamp-capsule">
+							Geschrieben am: <input type="text" class="editor-timestamp">
+						</div>
+					</div>
+					<div class="editor-right">
+						<div class="editor-right-top">
+							<button class="btn btn-form">Add Poetry</button>
+							<br />
+							<button class="btn btn-form">Clear</button>
+						</div>
+						<div class="editor-right-bottom">
+							<span class="editor-shareselect-head">Teilen auf:</span><br /> <input
+								type="checkbox" class="check" id="check-twitter" value="check-twitter" name="check-twitter" checked>Twitter<br />
+							<input type="checkbox" class="check" id="check-facebook" value="check-facebook" name="check-facebook" checked>Facebook<br />
+							<input type="checkbox" class="check" id="check-gplus" value="check-gplus" name="check-gplus" checked>Google+
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
