aa51a51128eddff9a594a7977128f39b0b3e172c
maqiv <sebastian.glinski@gmail.com>
2013-06-11 00:15:19 +0200

    commit to sync

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -28,39 +28,39 @@ function pickdate() {
 
 function initItems() {
 	$('.poetry-container-content #poetries').empty();
-	var it = localStorage["pstore.index"].split(";");
+	$('.poetry-container-content #drafts').empty();
+	var it = localStorage['pstore.index'].split(';');
 	
 	for (var i = 0; i < (it.length) - 1; i++) {
 		displayItem(it[i]);
 	}
+	publishItem();
 	removeItem();
 }
 
 function displayItem(index) {
-	var cont = "<div class=\"poetry-container-entry\" id=\"e_" + index + "\">";
-	cont += "<div class=\"poetry-container-entry-head\">" + localStorage["pstore." + index + ".title"] + "</div>";
-	cont += "<div class=\"poetry-container-entry-content\">" + localStorage["pstore." + index + ".title"] + "</div>";
-	cont += "<div class=\"poetry-container-entry-foot\">";
-	cont += "<div class=\"poetry-container-entry-foot-left\">Created on " + localStorage["pstore." + index + ".title"] + "</div>";
-	cont += "<div class=\"poetry-container-entry-foot-right\"><span class=\"poetry-container-entry-delete-click\" id=\"d_" + index + "\">L&ouml;schen</span></div>";
-	cont += "</div>";
-	cont += "</div>";
+	var cont = '<div class="poetry-container-entry" id="e_' + index + '">';
+	cont += '<div class="poetry-container-entry-head">' + localStorage['pstore.' + index + '.title'] + '</div>';
+	cont += '<div class="poetry-container-entry-content">' + localStorage['pstore.' + index + '.title'] + '</div>';
+	cont += '<div class="poetry-container-entry-foot">';
+	cont += '<div class="poetry-container-entry-foot-left">Created on ' + localStorage['pstore.' + index + '.title'] + '</div>';
+	cont += '<div class="poetry-container-entry-foot-right">' + ((localStorage['pstore.' + index + '.public'] == 'false') ? '<span class="poetry-container-entry-public-click">Ver&ouml;ffentlichen</span>' : '') + '<span class="poetry-container-entry-delete-click">L&ouml;schen</span></div>';
+	cont += '</div></div>';
 	
-//	if(localStorage["pstore." + index + ".public"] == )
-	$('.poetry-container-content #poetries').append(cont);
+	$('.poetry-container-content #' + ((localStorage['pstore.' + index + '.public'] == 'true') ? 'poetries' : 'drafts')).append(cont);
 }
 
 function refreshIndex() {
-	var s = "";
-	var t = "";
+	var s = '';
+	var t = '';
 	for (var i = 0; i < localStorage.length; i++) {
-		t = localStorage.key(i).split(".");
-		if (t[2] == "title")
+		t = localStorage.key(i).split('.');
+		if (t[2] == 'title')
 		{
-			s += t[1] + ";";
+			s += t[1] + ';';
 		}
 	}
-	localStorage["pstore.index"] = s;
+	localStorage['pstore.index'] = s;
 }
 
 function storeItem() {
@@ -68,24 +68,24 @@ function storeItem() {
 		var tit = $('#editor-title').val();
 		var cor = $('#editor-corpus').val();
 		var dat = $('#editor-date').val();
-		var ptw = $('#check-twitter').prop('checked');
-		var pfb = $('#check-facebook').prop('checked');
-		var pgp = $('#check-gplus').prop('checked');
+		var pub = $('#check-draft').prop('checked');
 		
-		if (tit == "" || cor == "" || dat == "") {
+		if (tit == '' || cor == '' || dat == '') {
 			alert("Bitte alle Felder ausf\u00fcllen!");
 			return;
 		}
 		
 		var id = countI();
-		localStorage["pstore." + id + ".title"] = tit;
-		localStorage["pstore." + id + ".text"] = cor;
-		localStorage["pstore." + id + ".timestamp"] = dat;
+		localStorage['pstore.' + id + '.title'] = tit;
+		localStorage['pstore.' + id + '.text'] = cor;
+		localStorage['pstore.' + id + '.timestamp'] = dat;
+		localStorage['pstore.' + id + '.public'] = pub;
 		
 		//felder leeren und editor einklappen
-		$('#editor-title').val("");
-		$('#editor-corpus').val("");
-		$('#editor-date').val("");
+		$('#editor-title').val('');
+		$('#editor-corpus').val('');
+		$('#editor-date').val('');
+		$('#check-draft').attr('checked', false);
 		$('#colme').collapse('hide');
 		
 		refreshIndex();
@@ -95,33 +95,39 @@ function storeItem() {
 
 function removeItem() {
 	$('.poetry-container-entry-delete-click').click(function() {
-		var t = ($(this).attr('id')).split("_");
+//		console.log($(this).parent('.poetry-container-entry'));
+		var t = ($(this).closest('.poetry-container-entry').attr('id')).split('_');
 		var id = t[1];
-		localStorage.removeItem("pstore." + id + ".title");
-		localStorage.removeItem("pstore." + id + ".text");
-		localStorage.removeItem("pstore." + id + ".timestamp");
-//		localStorage["pstore.pcount"]--;
+		localStorage.removeItem('pstore.' + id + '.title');
+		localStorage.removeItem('pstore.' + id + '.text');
+		localStorage.removeItem('pstore.' + id + '.timestamp');
+		localStorage.removeItem('pstore.' + id + '.public');
 		$('#e_' + id).remove();
 		refreshIndex();
 	});
 }
 
+function publishItem() {
+	$('.poetry-container-entry-public-click').click(function() {
+//		console.log($(this).closest('.poetry-container-entry').attr('id'));
+		var t = ($(this).closest('.poetry-container-entry').attr('id')).split('_');
+		console.log(t[1]);
+		var id = t[1];
+		localStorage['pstore.' + id + '.public'] = 'true';
+		refreshIndex();
+		initItems();
+		$('#mySexyTabs a[href="#poetries"]').tab('show');
+	});
+}
+
 function countI() {
-	var i = localStorage["pstore.pid"];
-//	var c = localStorage["pstore.pcount"];
-	if (isNaN(i) || i == "" || i == null) {
+	var i = localStorage['pstore.pid'];
+	if (isNaN(i) || i == '' || i == null) {
 		i = 0;
 	} else {
-		i = parseInt(localStorage["pstore.pid"]);
+		i = parseInt(localStorage['pstore.pid']);
 	}
-//	if (isNaN(c) || c == "" || c == null) {
-//		c = 1;
-//	} else {
-//		c = parseInt(localStorage["pstore.pcount"]);
-//	}
 	i++;
-//	c++;
-	localStorage["pstore.pid"] = i;
-//	localStorage["pstore.pcount"] = c;
+	localStorage['pstore.pid'] = i;
 	return i;
 }
